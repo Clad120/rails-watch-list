22ff6a3bef87a63cb2e968cac1c842e4384beaae
Selim Begag <moncompteps3@gmail.com>
2021-07-13 20:10:22 +0900

    tried to put on heroku

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,11 +1,15 @@
 class BookmarksController < ApplicationController
   def new
-    @list = Bookmark.new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+
     @movies = Movie.all
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list)
     else
